0a0013d92b1325c922225fda2fc5392179806564
nikki-thn <ntruong4@myseneca.ca>
2017-10-24 10:53:07 -0400

    Update Stations.h

--- Stations.h
+++ Stations.h
@@ -1,1 +1,16 @@
-#pragma once
+#ifndef W2_STATIONS_H
+#define W2_STATIONS_H
+
+
+namespace w2 {
+
+	class Stations  {
+		char* m_fileName;
+		int m_numStations;
+		//Station m_stations;
+
+	};
+}
+
+#endif
+
