717041bf05a2559d87d6e8a24ea3ab2d4a13829b
nikki-thn <ntruong4@myseneca.ca>
2017-12-22 09:44:09 -0500

    Create Multiple_inheritance.cpp

---
+++ Multiple_inheritance.cpp
@@ -0,0 +1,49 @@
+//not every language have multiple inheritance
+//MI refers to a class that is derived from more than one class (classes that are unrelated to the others) 
+
+#include <iostream>
+using namespace std;
+
+class A {
+    
+    public:
+    A() { cout << "A::A()" << endl; }
+    virtual void displayA() {
+        cout << "A::displayA()" << endl;
+    }
+};
+
+class B {
+   
+    public:
+    B() { cout << "B::B()" << endl; }
+    void displayB() {
+        cout << "B::displayB()" << endl;
+    }
+;
+
+//by default it is private, if we forgot we cannot access anything from base class
+class C : public A, public B {
+     
+     public:
+     C() : A(), B() { cout << "C()"; }
+     void displayA {
+         cout << "C::diplayA()" << endl;
+     }
+     void displayB {
+         cout << "C::displayB()" << endl;
+     }
+     
+     
+};
+
+int main () {
+
+     C instanceC;
+     instanceC.displayA(); //C::displayA will be call, shawdowing from class A
+     instanceC.A::displayA(); //A::displayA will be call, specifying
+     instanceC.displayB(); //C::displayB will be call, shadowing
+     
+     //ptrvirtual enable pointer will be created if we enable polymorphism
+     return 0;
+}
