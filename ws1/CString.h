9fbcb2a370ddb3f936a03fdf46487ce81cc26e1d
nikki-thn <ntruong4@myseneca.ca>
2018-01-28 17:57:26 -0500

    Update CString.h

--- ws1/CString.h
+++ ws1/CString.h
@@ -1,3 +1,11 @@
+/* \file CString.h
+* \brief header file for \class CString class, this file contains
+* the prototypes of CString data members and member functions
+* \author [Nikki Truong - 112 314 174 - OOP345 - Section C]
+* \date [Jan 28, 2018]
+*/
+
+
 #ifndef W1_CSTRING_H
 #define W1_CSTRING_H
 
@@ -5,23 +13,41 @@
 
 namespace w1 {
 
+	const int MAX = 3; //!< \var int MAX is maximum number of characters being displayed
+	
 	class CString {
 
-		int m_numOfChars;
-		char* m_string;
+		char* m_string; //!< \brief private member to store the processed string
 
 	public:
 
+		/*! \fn Default Constructor
+		 * Takes in zero parameter and initlize m_string to null
+		*/
 		CString();
-		CString(const char*, int);
-		~CString();
-		void display(std::ostream&) const;
-		//std::istream read(std::istream&);
 
+	    /*! \fn One parameter constructor
+		 * \param[in] [string] [a C-type string that is passed to be exact]
+		*/
+		CString(const char* string); 
+
+		/*! \fn Destructor
+		* \brief Clean up memory
+		*/
+		~CString();
+
+		/*! \fn display
+		* \para[in] [os] [takes in a reference of ostream to display the string
+		* to output stream]
+		*/
+		void display(std::ostream& os) const;
 	};
 
-	std::ostream& operator<< (std::ostream& os, const CString&);
-	//std::istream& operator>> (std::istream is, CString&);
+	/*! \fn overload of operator<< for \class CString 
+	* \para[in] [os] [takes in a reference of ostream to display the string
+	* to output stream]
+	*/
+	std::ostream& operator<< (std::ostream& os, const CString& aString);
 };
 
 #endif
