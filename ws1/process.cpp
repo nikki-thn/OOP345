c4b7a8252eccb93d4c461e866094360c820aa535
nikki-thn <ntruong4@myseneca.ca>
2018-01-28 17:54:42 -0500

    Update process.cpp

--- ws1/process.cpp
+++ ws1/process.cpp
@@ -1,14 +1,20 @@
+/* \file process.cpp
+* \brief this file contains the implementation of \fn toProcess()
+* \author [Nikki Truong - 112 314 174 - OOP345 - Section C]
+* \date [Jan 28, 2018]
+*/
+
 #include<iostream>
 #include"process.h"
 #include "CString.h"
 
-using namespace std;
-using namespace w1;
+/*! \fn toProcess()
+ * [This funtion will take in a C-type string, use CString class to exact
+ * and print out the string that is stored by CString]
+ * \param[in] [string] [a C-type string to be processed]
+ */
+void toProcess(const char* string) {
 
-void toProcess(const char* string, int max) {
-
-	CString aString(string, max);
-
-	cout << aString;
-
+	w1::CString aString(string);
+	std::cout << aString;
 }
