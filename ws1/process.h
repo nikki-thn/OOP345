b2f937114c9aa89382d1b8e618ee5899e7ad756e
nikki-thn <ntruong4@myseneca.ca>
2018-01-28 17:54:26 -0500

    Update process.h

--- ws1/process.h
+++ ws1/process.h
@@ -1,9 +1,19 @@
+/* \file process.h
+* \brief header file for process module, this file contains
+* the prototype for \fn toProcess()
+* \author [Nikki Truong - 112 314 174 - OOP345 - Section C]
+* \date [Jan 28, 2018]
+*/
+
 #ifndef PROCESS_H
 #define PROCESS_H
 
 #include "CString.h"
 
-void toProcess(const char* string, int max);
+	/*! \fn toProcess
+     * param[in] [string] [takes in a string to be processed]	
+	*/
+	void toProcess(const char* string);
 
 
 #endif
