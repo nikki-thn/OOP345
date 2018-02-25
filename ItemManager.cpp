49dd16ad27bf8f8c447d0e647e28e7fb6f770204
Nikki Truong <nikki@Nikkis-Mac-mini.local>
2018-02-24 19:40:42 -0500

    save

--- ItemManager.cpp
+++
@@ -1,9 +0,0 @@
-#include "ItemManager.h"
-
-
-void ItemManager::display(std::ostream& os, bool insertDescription) const {
-
-	for (auto& item : *this) {
-		item.display(os, insertDescription);
-	}
-}
